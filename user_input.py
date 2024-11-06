847d7b91c3bae1ead0dcd89d1f6cc467c56f5247
Adamu Peace <149883198+peace-adamu@users.noreply.github.com>
2024-11-05 19:02:28 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Use the input() function to ask the user for their name and store it in a variable called "name".
+name = input('Enter your name: ')
+
+# Use the input() function to ask the user for their age and store it in a variable called "age"
+age = input('What is your age: ')
+
+# Use the input() function to ask the user for their location and store it in a variable called "location".
+location = input('Give details of your location: ')
+
+# Print out a personalized message using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
+print(f"Hello {name}, you are {age} years old and live in {location}")
